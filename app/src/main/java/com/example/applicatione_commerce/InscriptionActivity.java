2536b562ac24a7a4fc67e948bbc609adf918f7c7
Sandra Vict0r <sandra.victor@outlook.fr>
2021-12-18 23:14:37 +0100

    Intent vers l'inscription

---
+++ app/src/main/java/com/example/applicatione_commerce/InscriptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.applicatione_commerce;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_inscr);
+    }
+}
--- app/src/main/java/com/example/applicatione_commerce/MainActivity.java
+++ app/src/main/java/com/example/applicatione_commerce/MainActivity.java
@@ -2,26 +2,35 @@ package com.example.applicatione_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.applicatione_commerce.R;
-import com.google.firebase.FirebaseApp;
+/*import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthCredential;*/
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.prefs.PreferenceChangeEvent;
 
 
 public class MainActivity extends AppCompatActivity {
-    GoogleCredential credential = GoogleCredential.fromStream(resourceAsStream);
+    //GoogleCredential credential = GoogleCredential.fromStream(resourceAsStream);
+    public void inscription(View v){
+        Intent intent = new Intent(this, InscriptionActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formulaireacceuilconnexion);
 
-        FileInputStream serviceAccount =
+        /*FileInputStream serviceAccount =
                 new FileInputStream("path/to/serviceAccountKey.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
@@ -29,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 .setDatabaseUrl("https://application-e-commerce-b47b4-default-rtdb.europe-west1.firebasedatabase.app")
                 .build();
 
-        FirebaseApp.initializeApp(options);
+        FirebaseApp.initializeApp(options);*/
     }
 
 
