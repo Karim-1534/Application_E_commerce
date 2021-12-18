b9f9a58047db0bd0a33924a75d7b8a4cc4dcb79b
Sandra Vict0r <sandra.victor@outlook.fr>
2021-12-19 00:28:25 +0100

    spinner de type de compte

--- app/src/main/java/com/example/applicatione_commerce/MainActivity.java
+++ app/src/main/java/com/example/applicatione_commerce/MainActivity.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +20,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.GoogleAuthCredential;*/
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.prefs.PreferenceChangeEvent;
 
 
@@ -25,10 +30,26 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, InscriptionActivity.class);
         startActivity(intent);
     }
+
+    public void connexion(View v){
+        Spinner spinner = (Spinner) findViewById(R.id.choixcatego);
+        TextView type = (TextView) spinner.getSelectedView();
+        Toast.makeText(this, type.getText(), Toast.LENGTH_LONG).show();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formulaireacceuilconnexion);
+        List<String> list = new ArrayList<String>();
+        list.add("Gestionnaire");
+        list.add("Commerçant");
+        list.add("Client");
+        Spinner spinner = (Spinner) findViewById(R.id.choixcatego);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,list);
+        spinner.setAdapter(adapter);
+
+
 
         /*FileInputStream serviceAccount =
                 new FileInputStream("path/to/serviceAccountKey.json");
