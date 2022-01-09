0ff7c40972847c148e27395be3cbefb17da93535
Karim-1534 <karim.siahmed17@gmail.com>
2022-01-10 00:55:28 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/deploymentTargetDropDown.xml
    #	app/src/main/java/com/example/applicatione_commerce/AjoutAnnonce.java

---
+++ app/src/main/java/com/example/applicatione_commerce/Model/Commandes/Commande.java
@@ -0,0 +1,66 @@
+package com.example.applicatione_commerce.Model.Commandes;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.ServerTimestamp;
+
+import java.util.List;
+
+public class Commande {
+    List<DocumentReference> PRODUITS;
+    DocumentReference CLIENT;
+    DocumentReference COMMERCANT;
+    Timestamp DATE;
+    String STATUT;
+
+    public Commande() {
+    }
+
+    public Commande(List<DocumentReference> PRODUITS, DocumentReference CLIENT, DocumentReference COMMERCANT, Timestamp DATE, String STATUT) {
+        this.PRODUITS = PRODUITS;
+        this.CLIENT = CLIENT;
+        this.COMMERCANT = COMMERCANT;
+        this.DATE = DATE;
+        this.STATUT = STATUT;
+    }
+
+    public List<DocumentReference> getPRODUITS() {
+        return PRODUITS;
+    }
+
+    public void setPRODUITS(List<DocumentReference> PRODUITS) {
+        this.PRODUITS = PRODUITS;
+    }
+
+    public DocumentReference getCLIENT() {
+        return CLIENT;
+    }
+
+    public void setCLIENT(DocumentReference CLIENT) {
+        this.CLIENT = CLIENT;
+    }
+
+    public DocumentReference getCOMMERCANT() {
+        return COMMERCANT;
+    }
+
+    public void setCOMMERCANT(DocumentReference COMMERCANT) {
+        this.COMMERCANT = COMMERCANT;
+    }
+
+    public Timestamp getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(Timestamp DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getSTATUT() {
+        return STATUT;
+    }
+
+    public void setSTATUT(String STATUT) {
+        this.STATUT = STATUT;
+    }
+}
